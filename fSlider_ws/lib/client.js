76aef964d90b3c92f63f332bb402d33831c8d44d
Ran <abbshr@outlook.com>
2014-07-09 00:23:06 +0800

    redefined the Payload_data exchange format standard

--- fSlider_ws/lib/client.js
+++ fSlider_ws/lib/client.js
@@ -1,30 +1,49 @@
 var encodeFrame = require('./encode.js');
 var decodeFrame = require('./decode.js');
 
+/* 
+* Constructor @socket: net.Socket instance
+* method:
+* .emit @e, @data: emit an event '@e' with '@data' to the client
+* .setTimeOut @tiemout
+*/
+module.exports = Client;
+
 /* ref of the client which connected to wsf Server */
 function Client(socket) {
   this.socket = socket;
+  this.ip = socket.remoteAddress;
+  this.port = socket.remotePort;
 }
 
-/*Client.prototype.on = function (e, callback) {
-  this.socket.on(e, callback);
+/* 
+* wsf Payload_data in application-level Exchange Standard format:
+*
+* <JSON>
+*
+* {
+*   'event'<String>: custom/system event name
+*   'data'<*>: real data to be sent
+* }
+*
+*/
+
+Client.prototype.setTimeOut = function (timeout) {
+  this.socket.setTimeOut(timeout);
 };
 
-Client.prototype.rm = function (e, callback) {
-  this.socket.removeListener(e, callback);
-}*/
-
 // emit event to current client
-// data format: JSON
 Client.prototype.emit = function (e, data) {
-  var payload_data_json = JSON.stringify({ event: e, data: data });
+  
+  // get the standard payload data format
+  var payload_data_json = JSON.stringify({ 'event': e, 'data': data });
   var frame = {
     FIN: 1,
     Opcode: 1,
     MASK: 0,
     Payload_data: payload_data_json
   };
-  this.socket.write(encodeFrame(frame));
-}
 
-module.exports = Client;
+  // if return false, pause the 'data' event handling progress
+  this.socket.write(encodeFrame(frame)) || this.socket.pause();
+}
