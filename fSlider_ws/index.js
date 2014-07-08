5a101b085b13bc78a1a74bb69bdc36e70c7d7abc
Ran <abbshr@outlook.com>
2014-07-09 00:22:07 +0800

    support more system event & rewrite the data reciver algorithm & add more comments

--- fSlider_ws/index.js
+++ fSlider_ws/index.js
@@ -9,10 +9,43 @@ var core         = require('./lib'),
 
 var MAGIC_STRING = '258EAFA5-E914-47DA-95CA-C5AB0DC85B11';
 
-var wsf          = {};
+var wsf = {};
 
 // inherint from EventEmitter
 wsf = Object.create(new EventEmitter());
+
+
+/*
+* wsf
+* method:
+* .connect: connect to a WebSocket Server
+* Class:
+* wsf.Server @server, @options: create a WebSocket Server
+* method:
+* .bind @server, @options: bind wsfserver to a http Server
+* .unbind: opposite to .bind(server, options)
+* .listen @callback: start listen on http server, add listener to 'upgrade' event
+* .broadcast @e, @data: broadcast to all clients
+* .on: (inherint from EventEmitter), on event triggered
+* .emit: (inherint from EventEmitter), trigger event
+* .removeListener: (inherint from EventEmitter)
+*/
+
+/*
+* wsf System level events, 
+* these events' name are important and shouldn't be overwrited or conflict in application
+*
+* #listen @httpServer: new http.Server()
+* #uptolimit @limit: new wsf.Server().MAX
+* #disconnected @client: new Client()
+* #closing @data: new Buffer()
+* #drained @bufferSize: socket.bufferSize
+* #timeout
+* #exception @err: new Error()
+* #connected @client: new Client()
+*
+*/
+module.exports = wsf;
 
 // as a Client
 wsf.connect = function () {};
@@ -74,20 +107,16 @@ wsf.Server.prototype.unbind = function () {
 * @callback
 */
 wsf.Server.prototype.listen = function (callback) {
-  this.on('listen', callback);
   var httpServer = this.httpServer;
+  if (callback instanceof Function)
+    this.on('listen', callback);
   if (!httpServer || !(httpServer instanceof http.Server)) 
     return new Error('no http server init');
-
   // bind 'upgrade' event callback
   httpServer.on('upgrade', upgrade_handler.bind(this));
-  this.emit('listen');
+  this.emit('listen', httpServer);
   return this;
 };
-
-/*wsf.Server.prototype.toPool = function (client, frame) {
-  this.emit
-}*/
 
 /*
 * #broadcast(e, data)
@@ -101,95 +130,128 @@ wsf.Server.prototype.broadcast = function (e, data) {
   });
 };
 
-module.exports = wsf;
-
 /* 'upgrade' event callback */
+/* this has been binded to Server instance */
+/* argument @req has been pre-setted */
 function upgrade_handler(req, socket) {
+  // ref to server
+  var server = this;
 
-  // up to limit, throw exception
-  if (this.sockets.length + 1 > this.MAX) 
-    return new Error('can not handle this request, socket has been up to the MAX number');
-
-  // 
-  if (req.url !== this.namespace) return;
-
-  var key = req.headers['sec-websocket-key'];
-  key = crypto.createHash('sha1')
-    .update(key + MAGIC_STRING)
-    .digest('base64');
-
-  /* ws protocol handshake request head */
-  var resHeaders = [
-    'HTTP/1.1 101 Switching Protocols',
-    'Upgrade: websocket',
-    'Connection: Upgrade',
-    'Sec-WebSocket-Accept: ' + key
-  ];
+  // create a instance for connecting client to restore vars and buffer
+  var client = new Client(socket);
+  var resKey, resHeaders;
 
   // recive-buffer, as ref 
   var buffer = {
     frame: new Buffer(0),
   };
 
-  // create a instance for connecting client to restore vars and buffer
-  var client = new Client(socket);
-  resHeaders = resHeaders.concat('', '').join('\r\n');
+  // up to limit, throw exception
+  if (server.sockets.length + 1 > server.MAX) 
+    server.emit('uptolimit', server.MAX);
+    return new Error('can not handle this request, socket has been up to the MAX number');
 
+  // if the request page URL not match, reject the request
+  if (req.url !== server.namespace) 
+    return;
+
+  resKey = crypto.createHash('sha1')
+    .update(req.headers['sec-websocket-key'] + MAGIC_STRING)
+    .digest('base64');
+
+  /* ws protocol handshake request head */
+  resHeaders = ([
+    'HTTP/1.1 101 Switching Protocols',
+    'Upgrade: websocket',
+    'Connection: Upgrade',
+    'Sec-WebSocket-Accept: ' + resKey
+  ]).concat('', '').join('\r\n');
+
+  socket.setEncoding('utf8');
   // on any data incomming
-  socket.on('data', data_handler.bind(this, client, buffer));
+  socket.on('data', data_handler.bind(server, client, buffer));
 
   // on disconnect
   socket.on('close', function (has_error) {
-    socket.destory();
-    var stack = this.sockets;
-
+    var stack = server.sockets;
     // pop from stack
     stack.forEach(function (client, i) {
       if (client.socket === socket) 
         stack.splice(i, 1);
     });
-    this.emit('disconnect', client);
+    // trigger 'disconnected' event
+    server.emit('disconnected', client);
+    if (has_error)
+      console.log('some problems happened during socket closing');
   });
 
   // client ready to disconnect
-  socket.on('end', function () {
+  socket.on('end', function (data) {
+    if (data) 
+      data_handler.bind(server, client, buffer)(data);
     socket.end();
+    server.emit('closing', data);
+  });
+
+  socket.on('drain', function () {
+    socket.resume();
+    server.emit('drained', socket.bufferSize);
+    console.log('system buffer has been drained');
+  });
+
+  socket.on('timeout', function () {
+    socket.end('connection closed by timeout');
+    server.emit('timeout');
+  });
+
+  socket.on('error',function (err) {
+    socket.destroy();
+    server.emit('exception', err);
+    console.log(err);
   });
 
   // add the client to clients-stack
-  this.sockets.push(client);
+  server.sockets.push(client);
 
   // send handshake response
-  socket.write(resHeaders);
+  socket.write(resHeaders) || socket.pause();
 
   // on connection established
-  this.emit('connect', client);
+  server.emit('connected', client);
 }
 
+/* this has been binded to Server instance */
+/* arguments @client and @buffer has been pre-setted */
 function data_handler(client, buffer, data) {
+  var readable_data, payload_data, event, rawdata;
+
   // concat the buffer with last time resolved frame
   // because sometimes a lot of 'data' event may be triggered in one time,
   // node treat them as one-time event,
   // if not do so, some data could be lost
   buffer.frame = Buffer.concat([buffer.frame, data]);
-  
-  var readable_data;
 
   // the "while loop" to get all right data remain in buffer
   while (readable_data = decodeFrame(buffer.frame)) {
-    // to string
-    //readable_data['Payload_data'] = readable_data['Payload_data'].toString();
-    // match JSON format data
-    /*if (readable_data['Payload_data'].match(/^{(.+)}$/)) { 
-      data = JSON.parse(readable_data['Payload_data']);
-      e = data['event'];
-      this.emit(e, data['data']);
-    }*/
-
-    // TODO: handle the JSON obj include readable frame and raw rest frame
-    // readable_data.frame
-    // don't forget to emit custom event here
-
+    // translate raw Payload_data to string
+    payload_data = readable_data['Payload_data'].toString();
+    // if payload_data format is not standard, JSON.parse will throw an error
+    try {
+      payload_data = JSON.parse(payload_data);
+    } catch (e) {
+      // cut this frame and skip to remain_frame
+      console.log(e);
+    }
+    event = payload_data.hasOwnProperty('event') ? payload_data['event'] : new Error('Payload_data translate error');
+    rawdata = payload_data.hasOwnProperty('data') ? new Buffer(payload_data['data']) : new Error('Payload_data translate error');
+    if (event instanceof Error) {
+      throw event;
+    } else if (rawdata instanceof Error) {
+      throw rawdata;
+    } else {
+      // if nothing goes wrong, emit event to Server
+      this.emit(event, rawdata);
+    }
     // the rest frame data
     buffer.frame = readable_data.remain_frame;
   }
