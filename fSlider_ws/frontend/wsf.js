f09d0a1b3ba9102b97aca1fdba7be7199c53c4bb
Ran <abbshr@outlook.com>
2014-07-09 16:47:09 +0800

    add a browser script lib to simpify wsf opeartion

---
+++ fSlider_ws/frontend/wsf.js
@@ -0,0 +1,118 @@
+/* frontend script for fSlider-WebSocketFramework */
+/*
+* wsf
+* method
+* .on: bind listener on system level event
+* .emit: trigger an event on wsf
+* .removeListener: opposite to method .on
+* .connect @url, @cb(wsf.socket): return wsf.socket
+* sys_event
+* #connect: on method .connect success
+* #disconnect: on connection closed
+* object
+* wsf.socket: returned by wsf.connect() or wsf.connect callback's first argument 
+* method
+* .on: bind listener on any event
+* .emit: emit an event to server on any level
+* .send: send normal data to server
+* .close: close the connection
+* sys_event
+* #open: on connection established
+* #close: on connection closed
+* #error: on error happened
+*/
+
+(function (global) {
+  global.WebSocket ||  new Error('only supported in explorer');
+
+  var wsf = { 
+    _events: {
+      'connect': [],
+      'disconnect': [] 
+    } 
+  };
+
+  wsf.on = function (eName, cb) {
+    this._events[eName] ? this._events[eName].push(cb) : (this._events[eName] = [cb]);
+  };
+
+  wsf.emit = function (eName, data) {
+    this._events[eName] && this._events[eName].forEach(function (cb) {
+      setTimeout(cb.bind(null, data), 0);
+    });
+  };
+
+  wsf.removeListener = function (eName, cb) {
+    var index = this._events[eName] && this._events[eName].indexOf(cb);
+    this._events[eName].splice(index, 1);
+  };
+
+  wsf.connect = function (ws_url, cb) {
+    var socket = { 
+      _events: { 
+        'open': [],
+        'close': [],
+        'error': [] 
+      } 
+    };
+    if (!ws_url) {
+      throw new Error('URL is needed');
+    } else if (!ws_url.match(/^(ws:\/\/)|(wss:\/\/)/)) {
+      throw new Error('Unknow ws URL pattern');
+    } else {
+      if (cb instanceof Function) 
+        // async
+        this.on('connect', cb);
+      socket.ws = new WebSocket(ws_url);
+      socket.ws.onmessage = function(e) {
+        var payload_data = JSON.parse(e.data);
+        var event = payload_data['event'];
+        var data = payload_data['data'];
+        socket._events[event] && socket._events[event].forEach(function (cb) {
+          setTimeout(cb.bind(null, data), 0);
+        });
+      };
+      socket.ws.onopen = function (e) {
+        socket._events['open'].forEach(function (cb) {
+          setTimeout(cb.bind(null, e), 0);
+        });
+      };
+      socket.ws.onclose = function (e) {
+        socket._events['close'].forEach(function (cb) {
+          setTimeout(cb.bind(null, e), 0);
+        });
+        wsf.emit('disconnect', socket);
+      };
+      socket.ws.onerror = function (e) {
+        socket._events['error'].forEach(function (cb) {
+          setTimeout(cb.bind(null, e), 0);
+        });
+      };
+      socket.on = function (e, cb) {
+        this._events[e] ? this._events[e].push(cb) : (this._events[e] = [cb]);
+      };
+      socket.emit = function (e, data, type) {
+        var payload_data = {
+          'event': e || '',
+          'data': data || '',
+          'type': type || 'string'
+        }, 
+        payload_data_json = JSON.stringify(payload_data);
+        this.ws.send(payload_data_json);
+        console.log(payload_data_json);
+      };
+      socket.send = function (data, type) {
+        this.emit("message", data, type);
+      };
+      socket.close = function () {
+        this.ws.close();
+      };
+      this.emit('connect', socket);
+      // sync
+      return socket;
+    }
+  };
+
+  global.wsf = wsf;
+
+})(this);
