3f4bed3cb530f119d008ff4c175fcb70918ba832
Ran <abbshr@outlook.com>
2014-07-09 23:05:09 +0800

    update example

--- test_server/app.js
+++ test_server/app.js
@@ -10,13 +10,12 @@ ws.on('connected', function(socket) {
   socket.setTimeout(0);
   ws.on('win', function(data) { 
     console.log(data);  
-    //socket.emit('gamewin' , {'name': 'Ran Aizen', 'say': 'hello Ran, you win' });
-    socket.emit('data', data);
+    socket.send(data);
   });
   ws.on('closing', function () {
     console.log('client close the connection');
   });
-  ws.on('message', function(data) {
+  ws.recive(function(data) {
     console.log(data);
     socket.emit('gamewin', data);
   });
