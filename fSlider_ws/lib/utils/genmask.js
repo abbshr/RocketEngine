12d8c1e7fa36f3db0ac80ffcfae89b6b94e65964
Ran <abbshr@outlook.com>
2014-07-19 14:59:05 +0800

    clean the workspace

--- fSlider_ws/lib/utils/genmask.js
+++
@@ -1,4 +0,0 @@
-
-/* genaretor Masking_key */
-var crypto = require('crypto');
-module.exports = crypto.randomBytes(4).toJSON();
--- fSlider_ws/lib/utils/typeOf.js
+++
@@ -1,3 +0,0 @@
-module.exports = function (obj) {
-  return Object.prototype.toString.call(obj).slice(8, -1).toLowerCase();
-};
