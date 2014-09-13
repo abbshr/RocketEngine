e081a40e9fbb445dad17fe67eed99bf51afaf716
Ran <abbshr@outlook.com>
2014-09-13 14:44:10 +0800

    fix bug in example 2

--- test/watcher/routes/users.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET users listing. */
-router.get('/asd', function(req, res) {
-  res.send('respond with a resource');
-});
-
-module.exports = router;
