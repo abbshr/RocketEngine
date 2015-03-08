5b46375a95131c400c5a494f7f625267af0dba23
Ran <abbshr@outlook.com>
2015-03-08 17:14:23 +0800

    add a sample test

--- test/test.js
+++ test/test.js
@@ -1,4 +1,13 @@
 var muk = require('muk');
 var rewire = require('rewire');
+var assert = require('assert');
 
-describe();
+var rocket = require('..');
+var http = require('http').createServer();
+describe('test server instance', function () {
+  it('.listen() callback with a same http server', function () {
+    rocket.listen(http, function (server) {
+      assert.equal(server, http);
+    });
+  });
+});
